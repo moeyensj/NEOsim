7e44873fc62c1b51d76d757ce36b4bd8e46dbd45
Joachim Moeyens <moeyensj@gmail.com>
2016-01-14 13:26:57 -0800

    fixed MopsReader usage

--- MopsPlotter.py
+++ MopsPlotter.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import MopsReader
+
 HW = 0.00005
 HL = 0.00005
 A = 1
@@ -10,9 +12,6 @@ def _plotprettymaker(fig, ax):
     ax.set_xlabel("RA", size=16);
     ax.set_ylabel("DEC", size=16);
     fig.set_size_inches(10,7);
-    #ax.set_xlim(ra[0],ra[1])
-    #ax.set_ylim(dec[0],dec[1]);
-    ax.set_title("Random Selection: %i objects, %s nights" % (1, 1), size=20)
 
 def _textLocation(ax):
     ymin, ymax = ax.get_ylim()
@@ -25,7 +24,6 @@ def plotData(dataframe):
     fig, ax = plt.subplots(1,1)
     _plotprettymaker(fig, ax)
     ax.scatter(np.array(dataframe['ra']), np.array(dataframe['dec']));
-    #fig.savefig("data.jpg")
 
 def plotTracklets(dataframe, trackletFiles):
 
@@ -37,9 +35,9 @@ def plotTracklets(dataframe, trackletFiles):
     tracklets = []
     tracklet_num = 0
 
-    for filein in trackletFiles:
-        for line in open(filein, 'r'):
-            tracklets = np.fromstring(line, sep=" ", dtype=int)
+    for trackletFile in trackletFiles:
+        for line in open(trackletFile, "r"):
+            tracklets = MopsReader.readTracklet(line)
             tracklet_num += 1
         
             p1 = dataframe[dataframe['diaid'] == tracklets[0]]
@@ -51,9 +49,7 @@ def plotTracklets(dataframe, trackletFiles):
             ax.arrow(float(p1['ra']), float(p1['dec']), dRa, dDec,
                 head_width=HW, head_length=HL, fc='k', ec='k', alpha=A)
 
-
     ax.text(_textLocation(ax)[0], _textLocation(ax)[1], 'Tracklets: ' + str(tracklet_num), size=18, color='r')
-    #fig.savefig("tracklets.jpg")
 
 def plotTracks(dataframe, trackFiles):
 
@@ -78,4 +74,4 @@ def plotTracks(dataframe, trackFiles):
             ax.plot(ra, dec, color='k',alpha=0.7)
             
     ax.text(_textLocation(ax)[0], _textLocation(ax)[1], 'Tracks: ' + str(track_num), size=16, color='r')
-    #fig.savefig("tracks.jpg")
+    
