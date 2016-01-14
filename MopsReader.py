60ada029a1d150459dbceeeb32f19da1b0ca9342
Joachim Moeyens <moeyensj@gmail.com>
2016-01-14 13:24:29 -0800

    added reading functions, changed tracks and tracklets to read line only

--- MopsReader.py
+++ MopsReader.py
@@ -1,17 +1,42 @@
+import os
 import numpy as np
 import pandas as pd
+import sqlite3 as sql
 
-def readTracklets(trackletFile):
-    trackletFileIn = open(trackletFile, 'r')
-    for tracklet in trackletFileIn:
-        diaids = tracklet.split(" ", dtype=int)[:-1]
-        return np.array(diaids)
+def readTracklet(tracklet):
+    return np.fromstring(tracklet, sep=" ", dtype=int) 
 
-def readTracks(trackFile):
-    trackFileIn = open(trackFile, 'r')
-    for tracklet in trackFileIn:
-        diaids = tracklet.split(" ", dtype=int)[:-1]
-        return np.array(diaids)
+def readTrack(track):
+    return np.fromstring(track, sep=" ", dtype=int)
 
 def readDetections(detFile):
-    return pd.read_csv(detFile, sep=' ', header=None, names=['diaid', 'obshistid', 'ssmid', 'ra', 'dec', 'mjd', 'mag', 'snr'])
+    return pd.read_csv(detFile, sep=" ", header=None, names=['diaid', 'obshistid', 'ssmid', 'ra', 'dec', 'mjd', 'mag', 'snr'], index_col='diaid')
+
+def readIds(ids):
+    return np.fromstring(track, sep=" ", dtype=int)
+
+def readDataIntoDataframe(dataFile):
+    return pd.read_csv(dataFile, sep=" ", header=None, names=['diaid', 'obshistid', 'ssmid', 'ra', 'dec', 'mjd', 'mag', 'snr'], index_col='diaid')
+
+def readDataIntoDatabase(dataFile, database, table):
+    con = sql.connect(database)
+    con.execute("""
+    CREATE TABLE %s (
+        diaid INTEGER,
+        obshistid INTEGER,
+        ssmid INTEGER,
+        ra REAL,
+        dec REAL,
+        mjd REAL,
+        mag REAL,
+        snr REAL
+    );
+    """ % (table))
+
+    df = readDataIntoDataframe(dataFile)
+    df.to_sql(table, con, if_exists="append")
+
+    con.close()
+    del df
+
+    return
