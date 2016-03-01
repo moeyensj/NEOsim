6aff0b93caf45bc9b1ec59a40ba83ea2ef45bbf2
Joachim Moeyens <moeyensj@gmail.com>
2016-03-01 09:20:57 -0800

    removed database function for overhaul

--- MopsReader.py
+++ MopsReader.py
@@ -15,29 +15,6 @@ def readIds(ids):
 def readDetectionsIntoDataframe(detsFile, header=None):
     return pd.read_csv(detsFile, header=header, names=['diaid', 'obshistid', 'ssmid', 'ra', 'dec', 'mjd', 'mag', 'snr'], index_col='diaid', delim_whitespace=True)
 
-def readDetectionsIntoDatabase(detsFile, database, table):
-    con = sql.connect(database)
-    con.execute("""
-    CREATE TABLE %s (
-        diaid INTEGER,
-        obshistid INTEGER,
-        ssmid INTEGER,
-        ra REAL,
-        dec REAL,
-        mjd REAL,
-        mag REAL,
-        snr REAL
-    );
-    """ % (table))
-
-    df = readDetectionsIntoDataframe(detsFile)
-    df.to_sql(table, con, if_exists="append")
-
-    con.close()
-    del df
-
-    return
-
 def readNight(detFile):
     return os.path.basename(detFile).split(".")[0]
 
