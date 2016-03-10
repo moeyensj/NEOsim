7fd94141f15584d7e02ec4049a61ffbad38e1d51
Joachim Moeyens <moeyensj@gmail.com>
2016-03-10 15:12:43 -0800

    added views and renamed functions

--- MopsDatabase.py
+++ MopsDatabase.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-def buildMainDatabase(database, outDir, overwrite=False):
+def buildTrackletDatabase(database, outDir, overwrite=False):
 
     database = os.path.join(os.path.abspath(outDir), "", database)
     con = sqlite3.connect(database)
@@ -41,6 +41,84 @@ def buildMainDatabase(database, outDir, overwrite=False):
             trackletId INTEGER,
             diaid INTEGER
         );
+        """)
+
+    print "Creating Tracklets view..."
+    con.execute("""
+        CREATE VIEW Tracklets AS
+        SELECT * FROM AllTracklets
+        WHERE createdBy = 1
+        """)
+
+    print "Creating CollapsedTracklets view..."
+    con.execute("""
+        CREATE VIEW CollapsedTracklets AS
+        SELECT * FROM AllTracklets
+        WHERE deletedBy = 2
+        OR createdBy = 2
+        """)
+
+    print "Creating PurifiedTracklets view..."
+    con.execute("""
+        CREATE VIEW PurifiedTracklets AS
+        SELECT * FROM AllTracklets
+        WHERE deletedBy = 3
+        OR createdBy = 3
+        """)
+
+    print "Creating FinalTracklets view..."
+    con.execute("""
+        CREATE VIEW FinalTracklets AS
+        SELECT * FROM AllTracklets
+        WHERE deletedBy = 4
+        OR createdBy = 4
+        """)
+
+    return con
+
+def buildTrackDatabase(database, outDir):
+
+    database = os.path.join(os.path.abspath(outDir), "", database)
+    con = sqlite3.connect(database)
+
+    print "Creating AllTracks table..."
+    con.execute("""
+        CREATE TABLE AllTracks (
+            trackId INTEGER PRIMARY KEY,
+            linkedObjectId INTEGER,
+            numLinkedObjects INTEGER,
+            numMembers INTEGER,
+            rms REAL,
+            window REAL,
+            startTime REAL,
+            endTime REAL,
+            subsetOf INTEGER, 
+            createdBy INTEGER,
+            deletedBy INTEGER
+        );
+        """)
+
+    print "Creating TrackMembers table..."
+    con.execute("""
+        CREATE TABLE TrackMembers (
+            trackId INTEGER,
+            diaId INTEGER
+        );
+        """)
+
+    print "Creating Tracks view..."
+    con.execute("""
+        CREATE VIEW Tracks AS
+        SELECT * FROM AllTracks
+        WHERE createdBy = 5
+        """)
+
+    print "Creating FinalTracks view..."
+    con.execute("""
+        CREATE VIEW FinalTracks AS
+        SELECT * FROM AllTracks
+        WHERE deletedBy = 6
+        OR createdBy = 6
         """)
 
     return con
